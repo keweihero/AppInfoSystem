f80534dd7b82b5545b5cde711204cc22b5502627
kewei_liao <kewei_liao@163.com>
2018-11-28 11:15:54 +0800

    SSM项目整合测试

--- src/main/java/com/bdqn/controller/DevController.java
+++ src/main/java/com/bdqn/controller/DevController.java
@@ -1,7 +1,14 @@
 package com.bdqn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/dev")
+@Controller
+@RequestMapping("/dev")
 public class DevController {
+
+    @RequestMapping("/login.action")
+    public String login(){
+        return "devlogin";
+    }
 }
--- src/main/java/com/bdqn/controller/ManagerController.java
+++ src/main/java/com/bdqn/controller/ManagerController.java
@@ -1,7 +1,14 @@
 package com.bdqn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("manager")
+@Controller
+@RequestMapping("/manager")
 public class ManagerController {
+
+    @RequestMapping("/login.action")
+    public String login(){
+        return "backendlogin";
+    }
 }
